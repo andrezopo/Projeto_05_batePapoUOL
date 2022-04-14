8f740e8274cde7c65d7e2044e385885c32678100
André Zopolato <andrezopo@gmail.com>
2022-04-13 23:27:33 -0300

    Enviando mensagens

--- scripts.js
+++ scripts.js
@@ -2,37 +2,46 @@ let mensagensDom = document.querySelector(".mensagens")
 let mensagens = [];
 let nome;
 
+
 function buscarMensagens() {
     let promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages")
     promise.then(renderizarMensagens);
 }
 
-function renderizarMensagens(resposta) {
-    console.log(resposta);
-    mensagens = resposta.data
+function povoarDOM(mensagens){
     for (i = 0; i < mensagens.length; i++) {
         if (mensagens[i].type === "status") {
             mensagensDom.innerHTML += `<div class="mensagem cinza"><span><h4>${mensagens[i].time}</h4><strong> ${mensagens[i].from}</strong> ${mensagens[i].text}</span></div>`
         }
         else if (mensagens[i].type === "message"){
-            `<div class="mensagem"><span><h4>${mensagens[i].time}</h4><strong> ${mensagens[i].from}</strong> para <strong>${mensagens[i].to}:</strong> ${mensagens[i].text}</span></div>`
+            mensagensDom.innerHTML += `<div class="mensagem"><span><h4>${mensagens[i].time}</h4><strong> ${mensagens[i].from}</strong> para <strong>${mensagens[i].to}:</strong> ${mensagens[i].text}</span></div>`
         }
         else {
-            `<div class="mensagem vermelho"><span><h4>${mensagens[i].time}</h4><strong> ${mensagens[i].from}</strong> para <strong>${mensagens[i].to}:</strong> ${mensagens[i].text}</span></div>`
+            mensagensDom.innerHTML += `<div class="mensagem vermelho"><span><h4>${mensagens[i].time}</h4><strong> ${mensagens[i].from}</strong> para <strong>${mensagens[i].to}:</strong> ${mensagens[i].text}</span></div>`
         }
     }
+}
+
+function renderizarMensagens(resposta) {
+    mensagens = resposta.data
+    povoarDOM(mensagens)
+    let ultimaMensagem = document.querySelector(".mensagens > :last-child");
+    ultimaMensagem.scrollIntoView();
 }
 
 function definirUsuario() {
     nome = prompt("Qual o nome de usuário?");
     let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", { name: nome });
-    promise.then(buscarMensagens);
+    promise.then(atualizarMensagens);
     promise.then(manterConexao);
     promise.catch(falhaUsername);
 
 }
 function manterConexao(){
-    let idInterval = setInterval(function () { axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", { name: nome });}, 180000);
+    let idInterval = setInterval(function () { axios.post("https://mock-api.driven.com.br/api/v6/uol/status", { name: nome });}, 4000);
+}
+function atualizarMensagens(){
+    let idInterval = setInterval(buscarMensagens, 3000);
 }
 
 function falhaUsername(erro) {
@@ -40,4 +49,18 @@ function falhaUsername(erro) {
         alert("Já existe um usuário online com este nome, escolha outro.")
         definirUsuario()
     }
+}
+
+function enviarMensagem() {
+    let mensagemAEnviar = document.querySelector("input");
+    let mensagem = {
+        from: nome,
+        to: "Todos",
+        text: mensagemAEnviar.value,
+        type: "message",
+    }
+    console.log(mensagem)
+    let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", mensagem);
+    promise.then(buscarMensagens);
+    mensagemAEnviar.value = null;
 }
